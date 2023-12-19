f5e126c9ee8710374a09256c14e39584248dbefd
Astro <rgrxmahabub.official@gmail.com>
2023-12-19 11:19:58 +0600

    Configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // add custom color here
+      colors: {
+        primary: "#3185fc",
+        secondary: "#1646cb",
+        white: "#ffffff",
+      },
+      // add custom font family here
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+});
